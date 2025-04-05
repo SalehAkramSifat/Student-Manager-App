ddaddf35e809f789fef008062fca25e69c859fc7
Saleh Akram Sifat <salehakramsifat@gmail.com>
2025-04-05 17:14:40 +0600

    Provider Setup

---
+++ lib/provider/student_provider.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
+import 'package:student_manager_app/model/student_model.dart';
+
+class StudentProvider with ChangeNotifier{
+  List<Students> _students = [];
+
+  List<Students> get students => _students;
+  void addStudent(Students students) {
+    _students.add(students);
+    notifyListeners();
+  }
+}
