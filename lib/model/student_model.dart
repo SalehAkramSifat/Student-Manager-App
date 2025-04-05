fe49f58d46a7b9e8ed229639cbbe2606afe3b605
Saleh Akram Sifat <salehakramsifat@gmail.com>
2025-04-05 17:14:23 +0600

    data model

---
+++ lib/model/student_model.dart
@@ -0,0 +1,15 @@
+class Students {
+  String name;
+  int age;
+  double score;
+
+  Students ({required this.name, required this.age, required this.score});
+
+  String getGreade(){
+    if (score >= 90) return 'A+';
+    else if (score >= 80) return 'A';
+    else if (score >= 70) return 'B';
+    else if(score >= 60) return 'C';
+    else return 'F';
+  }
+}
