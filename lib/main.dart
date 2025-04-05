4943dd6ee9b4486432ea7593e41bc581b902c83a
Saleh Akram Sifat <salehakramsifat@gmail.com>
2025-04-05 15:18:01 +0600

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:student_manager_app/provider/student_provider.dart';
+import 'package:student_manager_app/screens/home_screen.dart';
+
+main(){
+  runApp(ChangeNotifierProvider(
+    create: (context)=> StudentProvider(),
+  child: MyApp(),));
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
