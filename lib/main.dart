d87e2cba7060dea1d5dcc068df5a7d75c830337b
Saleh Akram Sifat <salehakramsifat@gmail.com>
2025-04-07 22:47:28 +0600

    Main update

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,15 @@ import 'package:student_manager_app/provider/student_provider.dart';
 import 'package:student_manager_app/screens/home_screen.dart';
 
 main(){
-  runApp(ChangeNotifierProvider(
-    create: (context)=> StudentProvider(),
-  child: MyApp(),));
+  runApp(
+      ChangeNotifierProvider(
+        create: (context)=> StudentProvider(),
+        child: MyApp(),));
 }
 
 class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
